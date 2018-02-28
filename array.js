f157e42ee943be16aa5a12224d5c61a968b7f817
Mehret Kulala <merutaye@gmail.com>
2018-02-27 20:28:17 -0600

    Compeleted excercise 1

---
+++ array.js
@@ -0,0 +1,13 @@
+console.log("h1");
+var test="cat";
+var  opp = test.split("").reverse().join("");
+// var opp = "tac";
+// ["c","a","t"] ["c]
+if (test == opp) {
+    console.log (test,true);
+} else {
+    console.log (test,false);
+}
+
+
+

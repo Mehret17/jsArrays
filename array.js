2fb513fe87919380ea79255f9bd376dba1102af0
Mehret Kulala <merutaye@gmail.com>
2018-02-27 21:20:35 -0600

    added challenge 2

--- array.js
+++ array.js
@@ -9,5 +9,21 @@ if (test == opp) {
     console.log (test,false);
 }
 
+var animals = ['fish', 'dog', 'cat'];
+var animalzElement = document.getElementById('animalz');
+for(var i=0; i<animals.length; i++){
+ // console.log("animals", animals[i]);
+ animalzElement.innerHTML += "<h5>" + animals[i] + "</h5>";
+}
 
 
+var book = "catch 22";
+var bookSplit = book.split("");
+var realNumz = [];
+for (var j=0; j<bookSplit.length; j++) {
+    // cosnole.log("bookSplit", bookSplit[j]);
+    if (bookSplit[j]*1){
+    realNumz.push(bookSplit[j]);
+    }
+}
+console.log("answer", realNumz.join(""))
